0a274dbe30ba4691570c05a95e9dad21772ee591
DemonKingSwarn <69480361+DemonKingSwarn@users.noreply.github.com>
2021-09-21 20:16:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let theme = "light";
+
+let light = {};
+light.background = "#ffffff";
+light.color = "#000000";
+light.anylink = "#08088A";
+light.themebutton = "Dark Mode";			
+
+let dark = {};
+dark.background = "#000000";
+dark.color = "#ffffff";
+dark.anylink = "#ff0000";
+dark.themebutton = "Light Mode";
+
+function SwitchTheme() {
+	let current = (theme === "light") ? dark : light;
+
+	document.body.style.background = current.background;
+	document.body.style.color = current.color;
+	document.getElementById("anylink").style.color = current.anylink;
+	document.getElementById("themebutton").text = current.themebutton;
+
+	theme = (theme === "light") ? "dark" : "light"; 
+}
+
+// Switch to dark theme as soon as page loads
+SwitchTheme();
